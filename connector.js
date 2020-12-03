6bea0698026e48cdc6710f9c89cce5a4e7cda3d6
toothsy <c.j.atharva@gmail.com>
2020-12-03 13:24:17 +0530

    mistakes Resolved

--- connector.js
+++ connector.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createPool({
     host: "bovjj5ckvtapvajqrfos-mysql.services.clever-cloud.com",
     user: "uquazjpy4t4uxomq",
-    password: "uquazjpy4t4uxomq",
+    password: "Wi9tzfZ2pTez3v2O23Ul",
     database: "bovjj5ckvtapvajqrfos",
     port:"3306",
     connectTimeout:"15000" 
--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const app = express()
 const port = process.env.PORT|| 3000
 const ga = require('./controllers/getArticles')
 app.get('/getArticles',ga.getArticles)
+app.get('/',(req,res)=>{res.send("please go to /getArticles")})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
