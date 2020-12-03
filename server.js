a4d0b3359e5be33a80cdb79adedb846c0f6a730e
toothsy <c.j.atharva@gmail.com>
2020-12-03 13:49:59 +0530

    redirection

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express()
 const port = process.env.PORT|| 3000
 const ga = require('./controllers/getArticles')
 app.get('/getArticles',ga.getArticles)
-app.get('/',(req,res)=>{res.send("please go to /getArticles")})
+app.get('/',(req,res)=>{res.redirect('/getArticles')})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
